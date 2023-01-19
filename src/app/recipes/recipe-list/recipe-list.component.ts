56d4e6e92ebb7f76bedfa489e398d51ca5aa6ec8
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2023-01-19 15:14:42 +0100

    recipe list in progress

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.models';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+  new Recipe('A Test Recipe','This is a real test','https://cdn.dribbble.com/users/2425253/screenshots/15276005/media/96f7e2bd3cc1089bcbf0eb26d74b4a08.jpg?compress=1&resize=768x576&vertical=top'),
+  new Recipe('A Test Recipe','This is a real test','https://cdn.dribbble.com/users/2425253/screenshots/15276005/media/96f7e2bd3cc1089bcbf0eb26d74b4a08.jpg?compress=1&resize=768x576&vertical=top')
+];
+
+constructor(){}
+
+ngOnInit(): void {
+  
+}
+
 
 }
